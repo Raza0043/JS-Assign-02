7962e6d939ba883053092d797ee1e0dbe413083a
Muhammad Ali Raza <155912766+Raza0043@users.noreply.github.com>
2024-08-30 03:20:00 +0500

    Add files via upload

---
+++ Assign#02/app.js
@@ -0,0 +1,304 @@
+// PDF_03
+
+// Question 01
+// var age="I am 20 years old"
+// alert(age)
+// Question 02
+// var anayliz="You have visited this website 20 times"
+// alert(anayliz)
+// Question 03
+// document.write("My birth is 2004 <br> Data type of declared variable is number")
+// Question 04
+// var visitor = "<b>Jhon Doe</b>";
+// var tshirts = " <b>5 t-shirts(s) </b>"
+// document.write(visitor + " ordered" + tshirts+"on <b>XYZ Clothing Store</b>")
+
+
+
+
+// PDF_04
+// Question # 01
+// var std_Name = "Muhammad Ali " , f_Name = "  Zahid Hussain ", std_Id = "Smit-261430 "  ;
+
+// Question # 02
+
+// :Legal Variables:
+
+// var std_id = "Raza";
+// var std1 = "Salman";
+
+// var std = "Ali";
+// var $std = "Kashif";
+
+// :illegal Variables:
+
+// var student name="123";
+// var 1std="Faisal";
+
+// var var =" Hello ";
+// var std-123=" Message box"
+
+// Question # 03
+
+// // (A)
+// var head1="Rules for naming JS variables";
+// // (B)
+// var text2="Variables name can only contain numbers,$ , _ for example : $my_1stVariable";
+// // (C)
+// var text3="Variable must begin with letter $ or _ .For example:$name,_name or name.";
+// // (D)
+// var text4="Variables names are case sensitive";
+// // (E)
+// var text5="Variables names should not be JS Keywords";
+
+// document.getElementById("heading").textContent = head1;
+// document.getElementById("para1").textContent = text2;
+// document.getElementById("para2").textContent = text3;
+// document.getElementById("para3").textContent = text4;
+// document.getElementById("para4").textContent = text5;
+
+// PDF_05 (Math Expressions)
+
+// Question # 01 (Addition)
+//  var a = 10;
+//  var b = 1;
+//   sum = a + b
+//  document.write( "Sum of 10 and 1 is " +sum)
+
+// Question # 02(Sub,Multi,Div and Modulus)
+// Subtraction
+//   var c = 10;
+//   var d = 1;
+// subt = c - d
+//   document.write( "Subtraction of 10 and 1 is " +   subt)
+// Multiple
+//   var e = 10;
+//   var f = 2;
+// mult = e *f
+//   document.write( "Multiplication  of 10 and 2 is " +   mult)
+
+// Division
+
+// var g = 10;
+// var h = 2;
+// Div = g / h;
+// document.write("Division  of 10 and 2 is " + Div);
+// Modulus
+// var i = 10;
+// var j = 3;
+// mod = i % j;
+// document.write("Modulus  of 10 and 2 is " + mod);
+// Quetion# 03
+
+// a.
+// var a;
+// document.write("<b>Value after variable declaration is:</b> " + a + "<br>");
+
+// // b.
+// a = 5;
+
+// // c.
+// document.write("<b>Initial value :</b> " + a + "<br>");
+
+// // e.
+// a++;
+
+// // f.
+// document.write("<b>Value after increment :</b> " + a + "<br>");
+
+// // g.
+// var b = 7;
+
+// // h.
+// var add = a + b;
+// document.write("<b>Value after addition :</b> " + add + "<br>");
+
+// // i.
+// add--;
+
+// // j.
+// document.write("<b>Value after decrement :</b> " + add + "<br>");
+
+// // k.
+// var rem = add % 3;
+
+// // l.
+// document.write("<b>The remainder is :</b> " + rem + "<br>");
+
+// Quetion# 04
+
+// var a=600;
+// var b= 5
+// var calculate =a*b;
+// document.write("Total cost to buy 5 tickets to a movie is:" +calculate +"PKR")
+
+// Quetion# 05
+
+// var a = 10;
+// var b = 1;
+// var mul = a * b;
+// document.write("10 x " + b + " = " + mul + "<br>");
+// var c = 10;
+// var d = 2;
+// var mult = c * d;
+// document.write("10 x " + b + " = " + mult + "<br>");
+// var e = 10;
+// var f = 3;
+// var multi = e * f;
+// document.write("10 x " + b + " = " + multi + "<br>");
+// var g= 10;
+// var h = 4;
+// var multip = g * h;
+// document.write("10 x " + b + " = " + multip + "<br>");
+// var i = 10;
+// var j = 5;
+// var multipl = i * j;
+// document.write("10 x " + b + " = " + multipl + "<br>");
+// var k = 10;
+// var l = 6;
+// var multipli = k * l;
+// document.write("10 x " + b + " = " + multipli + "<br>");
+// var m = 10;
+// var n = 7;
+// var multiplic = m * n;
+// document.write("10 x " + b + " = " + multiplic + "<br>");
+// var o = 10;
+// var p = 8;
+// var multiplica = o * p;
+// document.write("10 x " + b + " = " + multiplica + "<br>");
+// var q = 10;
+// var r = 9;
+// var multiplicati = q * r;
+// document.write("10 x " + b + " = " + multiplicati + "<br>");
+// var s = 10;
+// var t = 10;
+// var multipliaction = s * t;
+// document.write("10 x " + b + " = " + mul + "<br>");
+
+// Quetion# 06
+//  a
+//  var celsius = 25;
+
+//  // b
+//  var fahrenheit = (celsius * 9/5) + 32;
+//  document.write(celsius + "°C is " + fahrenheit + "°F<br>");
+
+//  //  c
+//  var fahrenheitTemp = 77;
+
+// // d
+//  var celsiusTemp = (fahrenheitTemp - 32) * 5/9;
+//  document.write(fahrenheitTemp + "°F is " + celsiusTemp.toFixed(2) + "°C<br>");
+// Quetion# 07
+
+//  var priceItem1 = 200;
+//  var priceItem2 = 150;
+//  var quantityItem_1 = 3;
+//  var quantityItem_2 = 2;
+//  var shippingCharges = 1000;
+
+//  // Calculate  cost
+//  var totalPriceItem1 = priceItem1 * quantityItem_1;
+//  var totalPriceItem2 = priceItem2 * quantityItem_2;
+//  var totalCost = totalPriceItem1 + totalPriceItem2 + shippingCharges;
+
+//  //  rceipt
+//  document.write("<h1>Shopping Cart Receipt</h1>");
+//  document.write("<p><b>Price of Item 1:</b> $" + priceItem1 + "</p>");
+//  document.write("<p><b>Ordered Quantity of Item 1:</b> " + quantityItem_1 + "</p>");
+//  document.write("<p><b>Total Price for Item 1:</b> $" + totalPriceItem1 + "</p>");
+
+//  document.write("<p><b>Price of Item 2:</b> $" + priceItem2 + "</p>");
+//  document.write("<p><b>Ordered Quantity of Item 2:</b> " + quantityItem_2 + "</p>");
+//  document.write("<p><b>Total Price for Item 2:</b> $" + totalPriceItem2 + "</p>");
+
+//  document.write("<p><b>Shipping Charges:</b> $" + shippingCharges + "</p>");
+//  document.write("<hr>");
+//  document.write("<p><b>Total Cost:</b> $" + totalCost + "</p>");
+
+// Quetion# 08
+
+//  var totalMarks = 1100;
+//  var marksObtained = 785;
+
+//  // percentage
+//  var percentage = (marksObtained / totalMarks) * 100;
+
+//  //  result
+//  document.write("<h1>Percentage Calculation</h1>");
+//  document.write("<p><b>Total Marks:</b> " + totalMarks + "</p>");
+//  document.write("<p><b>Marks Obtained:</b> " + marksObtained + "</p>");
+//  document.write("<p><b>Percentage:</b> " + percentage.toFixed(2) + "%</p>");
+
+// Quetion# 09
+
+//  var initialNumber = 2;
+
+// var result = ((initialNumber + 5) * 10) / 2;
+
+// document.write("<h1>Arithmetic Operations Result</h1>");
+// document.write("<p><b>Initial Number:</b> " + initialNumber + "</p>");
+// document.write("<p><b>Result after operations:</b> " + result + "</p>");
+
+// Quetion# 010
+
+ // a. 
+//  var currentYear = 2024;
+
+//  // b. 
+//  var birthYear = 2004 
+
+//  // c. 
+//  var ageInCurrentYear = currentYear - birthYear;
+//  var ageInNextYear = ageInCurrentYear + 1;
+
+//  document.write("<h1>Age Calculator</h1>");
+//  document.write("<p>They are either " + ageInCurrentYear + " or " + ageInNextYear + " years old.</p>");
+
+// Quetion# 012
+
+  //  // a. 
+  //  var radius = 5; 
+
+  //  //  value of π (pi)
+  //  var pi = 3.142;
+
+  //  // b. 
+  //  var circumference = 2 * pi * radius;
+
+  //  // Output of circumference
+  //  document.write("<p>The circumference is " + circumference.toFixed(2) + "</p>");
+
+  //  // c.
+  //  var area = pi * radius * radius;
+
+   
+  //  document.write("<p>The area is " + area.toFixed(2) + "</p>");
+
+  // Quetion# 013
+
+  // // a. 
+  // var favoriteSnack = "Chips";
+
+  // // b.
+  // var currentAge = 20; 
+
+  // // c. 
+  // var maxAge = 60; 
+
+  // // d. 
+  // var estimatedAmountPerDay = 2; 
+
+  // // e.
+  // var yearsLeft = maxAge - currentAge; 
+  // var totalAmount = yearsLeft * 365 * estimatedAmountPerDay; 
+
+  
+  // document.write("<p>You will need " + totalAmount + " " + favoriteSnack + " to last you until the ripe old age of " + maxAge + ".</p>");
+
+
+
+  // Pdf 6
+
+
+ 
